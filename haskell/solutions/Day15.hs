c64d59e9bef81ea3513a6c441c3eb8a994ccfe5a
Solonarv <nstamm@gmx.de>
2020-12-15 18:38:05 +0100

    Make day 15 F A S T

--- haskell/solutions/Day15.hs
+++ haskell/solutions/Day15.hs
@@ -1,33 +1,29 @@
 {-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 module Day15 where
 
-import Debug.Trace
+import Control.Monad.ST
+import Data.STRef
+-- import Debug.Trace
 
-import Control.Lens
-import Control.Monad.State
-import Data.IntMap (IntMap)
+import Control.Monad.Loops (whileM_)
+-- import Data.Vector.Unboxed.Mutable (MVector)
+import qualified Data.Vector.Unboxed.Mutable as MVector
+-- import qualified Data.Vector.Unboxed as Vector
+-- import Data.IntMap (IntMap)
 import qualified Data.IntMap.Strict as IntMap
 
 import AOC.Solution
 import ParsingPrelude
 import Util
 
-data Memory = Memory
-  { _memStart :: [Int]
-  , _memLastNumber :: Int
-  , _memNow :: Int
-  , _memSeen :: IntMap Int
-  }
-makeLenses ''Memory
-
 solution :: Solution [Int] Int Int
 solution = Solution
   { decodeInput = decimal `sepBy` ","
   , solveA = defSolver
-    { solve = Just . streamAt (2020-1) . generateVanEckSeq -- 0-baseed vs. 1-based indexing, grr!
+    { solve = Just . nthVanEck 2020
     }
   , solveB = defSolver
-    { solve = Just . streamAt (30000000-1) . generateVanEckSeq
+    { solve = Just . nthVanEck 30000000
     }
   , tests =
     [ "0,3,6" :=> [(PartA, "436"), (PartB, "175594")]
@@ -40,8 +36,11 @@ solution = Solution
     ]
   }
 
+-- pure implementation
+-- slow and kind of messy
 generateVanEckSeq :: [Int] -> Stream Int
-generateVanEckSeq (x:xs) = goStart IntMap.empty 1 x xs
+generateVanEckSeq [] = error "empty input"
+generateVanEckSeq (n:ns) = goStart IntMap.empty 1 n ns
   where
     goStart _ _ _ [] = error "empty input"
     goStart !seen !turn !lastNumber [x] = lastNumber :>> go (IntMap.insert lastNumber turn seen) (turn+1) x
@@ -52,3 +51,36 @@ generateVanEckSeq (x:xs) = goStart IntMap.empty 1 x xs
         Just prev -> turn - prev
       seen' = IntMap.insert lastNumber turn seen
       in go seen' (turn+1) next
+
+-- fast implementation
+-- tedious and hellish to debug
+-- unboxed ST go brr
+nthVanEck :: Int -> [Int] -> Int
+nthVanEck n start
+  | Just r <- safeIndex (n-1) start = r
+  | otherwise = runST do
+    seen <- MVector.new (maximum (n : start)+1)
+    MVector.set seen 0
+    turnR <- newSTRef 1
+    lastR <- newSTRef undefined
+    doStart seen turnR lastR start
+    whileM_ ((< n) <$> readSTRef turnR) do
+      lastVal <- readSTRef lastR
+      turn <- readSTRef turnR
+      lastSeen <- MVector.read seen lastVal
+      MVector.write seen lastVal turn
+      let out = if lastSeen == 0 then 0 else turn - lastSeen
+      writeSTRef turnR $! (turn+1)
+      writeSTRef lastR $! out
+    readSTRef lastR
+  where
+    doStart _ _ _ [] = error "empty input"
+    doStart _ _ lastR [x] = writeSTRef lastR $! x
+    doStart seen turnR lastR (x:xs) = do
+      turn <- readSTRef turnR
+      MVector.write seen x turn
+      writeSTRef turnR $! (turn+1)
+      doStart seen turnR lastR xs
+      
+
+

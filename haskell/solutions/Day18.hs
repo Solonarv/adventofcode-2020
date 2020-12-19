9d2da0b72ed42796158ea80abadb63cde8e41f0a
Solonarv <nstamm@gmx.de>
2020-12-19 04:15:18 +0100

    Day 18: remove expression AST

--- haskell/solutions/Day18.hs
+++ haskell/solutions/Day18.hs
@@ -11,10 +11,10 @@ solution :: Solution [[Token]] Int Int
 solution = Solution
   { decodeInput = some tok `sepBy` eol
   , solveA = defSolver
-    { solve = fmap (sum' . fmap eval) . traverse (parseMaybe flatExpr)
+    { solve = fmap sum' . traverse (parseMaybe flatExpr)
     }
   , solveB = defSolver
-    { solve = fmap (sum' . fmap eval) . traverse (parseMaybe precExpr)
+    { solve = fmap sum' . traverse (parseMaybe precExpr)
     }
   , tests =
     [ "1 + 2 * 3 + 4 * 5 + 6" :=> [(PartA, "71"), (PartB, "213")]
@@ -38,31 +38,24 @@ tok = lexeme noeol $ choice
   , TLit <$> decimal
   ]
 
-data Expr = Lit Int | Add Expr Expr | Mul Expr Expr
-  deriving (Eq, Ord, Show)
 
-flatExpr :: Parsec () [Token] Expr
-flatExpr = atom flatExpr `chainl` op
-  where
-    op = choice
-      [ Add <$ single TAdd
-      , Mul <$ single TMul
-      ]
-
-eval :: Expr -> Int
-eval (Lit x) = x
-eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y
-
-atom :: Parsec () [Token] Expr -> Parsec () [Token] Expr
+atom :: Parsec () [Token] Int -> Parsec () [Token] Int
 atom expr = choice
       [ token
-          \case TLit x -> Just (Lit x); _ -> Nothing
+          \case TLit x -> Just x; _ -> Nothing
           mempty
       , between (single TOpenParen) (single TCloseParen) expr
       ]
 
-precExpr :: Parsec () [Token] Expr
-precExpr = term `chainl` (Mul <$ single TMul)
+flatExpr :: Parsec () [Token] Int
+flatExpr = atom flatExpr `chainl` op
   where
-    term = atom precExpr `chainl` (Add <$ single TAdd)
+    op = choice
+      [ (+) <$ single TAdd
+      , (*) <$ single TMul
+      ]
+
+precExpr :: Parsec () [Token] Int
+precExpr = term `chainl` ((*) <$ single TMul)
+  where
+    term = atom precExpr `chainl` ((+) <$ single TAdd)
